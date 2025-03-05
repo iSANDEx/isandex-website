76d2c6fff8a598454f667279439c9c027ed4c52c
Jose L. Ulloa <84105116+jlulloaa@users.noreply.github.com>
2025-03-05 13:08:02 -0300

    added custom domain support
    configure the repo to host the page in a custom domain, rather than github's default

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function About() {
-    return (
+  const { t } = useTranslation(['translation']);
+  return (
   <section id="about" className="page-section" style={{ backgroundColor: '#6a93d4' }}>
     <div className="container">
       <h2 className="text-center text-uppercase text-dark-gray">About Us</h2>
-      <p className="text-muted text-center">We are experts in scientific software development, algorithm implementation, and medical image analysis. Our innovative solutions help advance the healthcare industry.</p>
-      {/* Add more content about the company here */}
+      <p className="text-center">
+        {t("sections.aboutus.part1")}
+      </p>
+      <p className="text-center">
+        {t("sections.aboutus.part2")}
+      </p>
     </div>
   </section>
 );
